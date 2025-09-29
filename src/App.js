2746547633b64ccb15566585d5b3a0dc8fbc00dd
Maria Melo <maria.melo@liferay.com>
2025-09-29 14:48:24 -0300

    clean App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React, {useEffect, useState} from 'react';
 
 import AddUserForm from './AddUserForm';
+import Blogs from './Blogs';
 
 import {getUsers} from './request';
 
 function App() {
-	const [users, setUsers] = useState([]);
-
-	useEffect(() => {
-		getUsers().then(res => {
-			setUsers(res.items);
-		})
-	}, []);
 
 	return (
 		<div className="row">
-			<div className="col-4">
-				<h1>Users:</h1>
-
-				{users.map(user => (
-					<div key={user.id}>
-						{user.givenName} {user.familyName}
-					</div>
-				))}
-			</div>
 			<div className="col-8">
-				<AddUserForm />
+				<Blogs />
 			</div>
 		</div>
 	);
